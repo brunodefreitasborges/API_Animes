7f032c82a1319a9876cb2ea5cd22ae63ed24b76b
Rafael Vaccaro <89113057+RafaelVaccaro@users.noreply.github.com>
2024-11-07 14:11:37 -0300

    add spring initializer

---
+++ anime-library-api/src/main/java/com/api/anime/anime_library_api/AnimeLibraryApiApplication.java
@@ -0,0 +1,13 @@
+package com.api.anime.anime_library_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimeLibraryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimeLibraryApiApplication.class, args);
+	}
+
+}
---
+++ anime-library-api/src/test/java/com/api/anime/anime_library_api/AnimeLibraryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.anime.anime_library_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeLibraryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
